03bfeb17962a52abfeee07438ca162f604a35720
SHAIK MAIRAJ <shaikmairajs@gmail.com>
2026-02-22 16:26:16 +0530

    rectifying 404 error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: false,
+    middleware: [
+      (req, res, next) => {
+        if (req.url.startsWith('/') && !req.url.includes('.') && !req.url.startsWith('/api')) {
+          req.url = '/index.html'
+        }
+        next()
+      }
+    ]
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
